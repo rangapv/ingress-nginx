f0b4a80fc4597e0a04903fe7ae10f55c4b46effb
rangapv <rangapv@yahoo.com>
2022-11-24 13:18:23 +0000

    Added pre-flight checks

--- nginx-ingress:2.4.1/kubeapply.sh
+++ nginx-ingress:2.4.1/kubeapply.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-f1="/home/ubuntu/uping/nginx-ingress:2.4.1/*"
+source <(curl -s https://raw.githubusercontent.com/rangapv/kubestatus/main/ks.sh) > /dev/null 2>&1
+count=0
+gcount=0
 
 func() {
 
 t1="$@"
-#echo "t1 is $t1"
 
 for k in $t1 
 do
   if [[ -d "$k" ]]
   then
-   # echo "Before recursion $count and k is $k"
     ((count+=1))
     func $k/*
-elif [[ -f "$k" ]] && (echo "$k" |  grep ".yaml" ) || (echo "$k" | grep ".yml")
+elif [[ -f "$k" ]] && (echo "$k" |  grep ".yaml" >>/dev/null 2>&1) || (echo "$k" | grep ".yml" >>/dev/null 2>&1)
 #elif [ -f "$k" ] && ( echo "$k" | awk '{split($0,a,"."); if (a[2] = "yaml") print a[2] }')
   then
     g1=`kubectl apply -f $k`
-    echo "g1 is $g1"
+    echo "$g1"
     # echo "$k" | ( grep ".yaml" || grep ".yml" ) 
     #echo "$k" | awk '{split($0,a,"."); if (a[2] = "yaml" || a[2] = "yml") print a[1] a[2] }'
     ((gcount+=1))
-    #cp -r $k $dest
   fi
 done
 }
 
+
+if [[ ($master -eq 1) || ($node -eq 1) ]]
+then
 func ./*
 echo "the Total file applied are  $gcount"
 echo "the Total Directory traversed are $count"
+else
+	echo "Kubernetes is not running on this box to install Nginx-Ingress controller..aborting install"
+fi
